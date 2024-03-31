85a78aa896586c57768bee429fabbde6f06708fe
merlin9341 <felixmlroughton@gmail.com>
2024-03-31 19:30:05 +0100

    Examples of operations. Chapter 3 p39

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-fn main() {
-    let mut x = 5;
+fn main() { 
     /*
+    let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
-    println!("The value of x is: {x}");*/
+    println!("The value of x is: {x}");
 
     let x = x + 1;
 
@@ -12,5 +12,29 @@ fn main() {
         println!("The value of x in the inner scope is: {x}");
     }
 
-    println!("The vale of x is: {x}")
+    println!("The vale of x is: {x}")*/
+
+    //FLoats
+
+    let x = 2.0; // f64
+
+    let y: f32 = 3.0; // f32
+
+    //Numeric operations
+    
+    // Addition
+    let sum = 5 + 10;
+
+    // Subtraction 
+    let difference = 95.5 - 4.3;
+
+    // Multiplication
+    let product = 4 * 30;
+
+    // Division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Resutls in -1
+
+    // remainder 
+    let remainder = 43 % 5;
 }
